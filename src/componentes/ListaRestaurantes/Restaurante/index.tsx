49a8d241ec2846f3a9a41f4f7f2c3f676c4b36ae
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-10-06 19:02:18 -0300

    Replicando o consumo da API para os pratos

--- src/componentes/ListaRestaurantes/Restaurante/index.tsx
+++ src/componentes/ListaRestaurantes/Restaurante/index.tsx
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import IPrato from '../../../interfaces/IPrato';
 import IRestaurante from '../../../interfaces/IRestaurante';
 import Prato from '../Prato';
 import estilos from './Restaurante.module.scss';
@@ -6,14 +9,34 @@ interface RestauranteProps {
   restaurante: IRestaurante
 }
 
+
+
 const Restaurante = ({ restaurante }: RestauranteProps) => {
+
+  const [pratos, setPratos] = useState<IPrato[]>()
+
+  
+  useEffect(() => {
+  
+    axios.get<IPrato[]>(`http://localhost:8000/api/v1/restaurantes/${restaurante.id}/pratos/`)
+  
+      .then(resposta => {
+        setPratos(resposta.data)
+      })
+  
+      .catch(erro => {
+        console.log(erro)
+      })
+  }, [restaurante.id])
+
+
 
   return (<section className={estilos.Restaurante}>
     <div className={estilos.Titulo}>
       <h2>{restaurante.nome}</h2>
     </div>
     <div>
-      {restaurante.pratos?.map(item => <Prato prato={item} key={item.id} />)}
+      {pratos?.map(item => <Prato prato={item} key={item.id}  />)}
     </div>
   </section>)
 }
